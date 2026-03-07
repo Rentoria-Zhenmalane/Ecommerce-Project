abe591fefdb914cb827354eca8dc0d20bbef5c7c
Shane <zhenren1098@gmail.com>
2026-03-07 14:33:31 +0800

    Installed Router package then created multiple pages using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} /> { /* path is the last part of the URL. HomePage has empty URL so set it to / .  element = which component to display*/}
 
+
+      <Route path="checkout" element={<div>test check out page</div>}></Route>
+    </Routes>
   )
 }
 
 export default App
 //5:19
+// Route requires 2 props: path and element
+
