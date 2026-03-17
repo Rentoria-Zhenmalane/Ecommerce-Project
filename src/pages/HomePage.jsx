ed38bcbf6284f89080632d3272b08efa1b078b98
Shane <zhenren1098@gmail.com>
2026-03-17 15:30:16 +0800

    Add server proxy config for /api and /images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,12 +16,12 @@ export function HomePage() {
   -We put it in useEffect to control how often we want it to run which is once only */ }
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
       });
 
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
       .then((response) => {
         setCart(response.data);
       });
